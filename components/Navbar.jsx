21d6258b2ccfb2d3405b9566afa63236f16fe6b9
Sumitha Sreekumar <sumisree502@gmail.com>
2024-07-09 16:41:11 +0530

    name

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">VS Headphones</Link>
+        <Link href="/">VS Electronics</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
